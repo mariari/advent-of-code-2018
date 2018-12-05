38cb020395ae1cbbdb7041da806c8d275c8b2b4a
mariari <nymphet@protonmail.com>
2018-12-05 01:33:39 -0500

    Played with style, last commit for the night

--- day4/day4.ml
+++ day4/day4.ml
@@ -67,12 +67,12 @@ let guard_sleep_times_of_info = function
        | {action = Guard num} :: xs -> loop num acc xs
        | {action = Sleep; time = start } :: {action = Wake; time = end_time} :: xs ->
           loop num
-               ({start
+               ({ start
                 ; num
-                ; length = (time_to_mins end_time - time_to_mins start) }
+                ; length = time_to_mins end_time - time_to_mins start }
                 :: acc)
                xs
-       | _ -> failwith "Invalid input format, an awake must come after alseep"
+       | _ -> failwith "Invalid input format, an awake must come after asleep"
      in
      loop num [] xs
   | {action = Sleep} :: _ | {action = Wake} :: _ -> failwith "A guard should come first"
@@ -82,9 +82,8 @@ let gen_minute_list start length =
   List.init length (fun i -> (start + i) mod 60)
 
 let increment_map map =
-  List.fold_left
-    ~init:map
-    ~f:(fun map x -> Map.update map x ~f:(Option.value_map ~default:1 ~f:succ))
+  List.fold_left ~init:map
+                 ~f:(Map.update ~f:(Option.value_map ~default:1 ~f:succ))
 
 let guard_sleep_times =
   let empty_map = Map.empty (module Int) in
@@ -93,10 +92,9 @@ let guard_sleep_times =
     ~f:(fun map {start; length ; num} ->
       let min_list = gen_minute_list start.minute length in
       Map.update map num
-        ~f:(Option.value_map
-              ~default:(length, increment_map empty_map min_list)
-              ~f:(fun (num_slept, map) ->
-                num_slept + length, increment_map map min_list)))
+        ~f:(function
+          | None                  -> length, increment_map empty_map min_list
+          | Some (num_slept, map) -> num_slept + length, increment_map map min_list))
 
 let solve_gen file =
   In_channel.read_lines file
@@ -106,22 +104,20 @@ let solve_gen file =
   |> guard_sleep_times
 
 let gratest_slept =
-  Map.fold
-    ~init:(0,0)
-    ~f:(fun ~key ~data (k,greatest) ->
-      if data > greatest
-      then (key,data)
-      else (k, greatest))
+  Map.fold ~init:(0,0)
+           ~f:(fun ~key ~data (k,greatest) ->
+             if data > greatest
+             then (key,data)
+             else (k, greatest))
 
 
 let solve_p1 file =
   solve_gen file
-  |> Map.fold
-      ~init:(0, 0, Map.empty (module Int))
-      ~f:(fun ~key ~data:(num_slept, slept_map) ((max_key, max_slept, m) as acc)
-          -> if num_slept > max_slept
-            then (key, num_slept, slept_map)
-            else acc )
+  |> Map.fold ~init:(0, 0, Map.empty (module Int))
+              ~f:(fun ~key ~data:(num_slept, slept_map) (max_key, max_slept, m as acc)
+                  -> if num_slept > max_slept
+                     then (key, num_slept, slept_map)
+                     else acc )
   |> fun (guard_num, _, map) ->
     let (key,_) = gratest_slept map in
     guard_num * key
@@ -129,12 +125,11 @@ let solve_p1 file =
 
 let solve_p2 file =
   solve_gen file
-  |> Map.fold
-      ~init:(0,0,0)
-      ~f:(fun ~key:curr_guard ~data:(_, slept_map) (guard, minute, times_slept) ->
-        let (time,curr_times_slept) = gratest_slept slept_map in
-        if curr_times_slept > times_slept then
-          (curr_guard, time, curr_times_slept)
-        else
-          (guard, minute, times_slept))
+  |> Map.fold ~init:(0,0,0)
+              ~f:(fun ~key:curr_guard ~data:(_, slept_map) (guard, minute, times_slept) ->
+                let (time,curr_times_slept) = gratest_slept slept_map in
+                if curr_times_slept > times_slept then
+                  (curr_guard, time, curr_times_slept)
+                else
+                  (guard, minute, times_slept))
   |> fun (guard, minute, _) -> guard * minute
